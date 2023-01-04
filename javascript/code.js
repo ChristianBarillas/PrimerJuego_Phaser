4f776bdccf39703db292fa2a3471cbe868382b85
christianbarillas <christianbarillasyt@gmail.com>
2023-01-04 13:58:11 -0600

    agregando reglas

--- javascript/code.js
+++ javascript/code.js
@@ -4,8 +4,8 @@
 var config = {
 
     type: Phaser.AUTO,
-    height: 800,
-    width: 600,
+    width: 800,
+    height: 500,
     physics : {
         default : 'arcade',
 
@@ -27,14 +27,17 @@ var config = {
 
 
 
-const game = new Phaser.Game(config)
+let game = new Phaser.Game(config)
 
 
 function preload () {
 
+    //recursos
+
     this.load.image('sky','/assets/fondo1.png')
     this.load.image('plataforma', '/assets/plataforma.png' )
     this.load.image('plataforma2', 'assets/plataforma2.png' )
+    this.load.spritesheet('zelda','assets/personaje.png', {frameWidth:120, frameHeight:120});
 
 
 
@@ -44,15 +47,49 @@ function preload () {
 
 function create ()  {
 
-    this.add.image(1773,800, 'sky');
+
+    //fondo del juego
+
+    this.add.image(1773,100, 'sky');
+
+
+    //plataforma
    
 plataform = this.physics.add.staticGroup ();
 
-plataform.create(200,600, 'plataforma').setScale(1.6);
-plataform.create(350, 250,'plataforma2').setScale(0.3);
+plataform.create(300,450, 'plataforma').setScale(0.2).refreshBody();
+plataform.create(350, 150,'plataforma2').setScale(0.2).refreshBody();
+
+
+
+
+//jugador
+
+player = this.physics.add.sprite(200, 200, 'zelda').setScale(0.4);
+player.setCollideWorldBounds(true);
+player.setBounce(0.4);
+
+
+// animaciones
+
+this.anims.create({
+
+    key: 'left',
+    frame: this.anims.generateFrameNumbers('zelda',{start:0 , end: 4})
+
+
+})
 
 
 }
+
+
+
+
+
+
+
+
 
 function update (){
 
